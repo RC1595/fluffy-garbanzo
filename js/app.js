2c990608d018488d1cb78c5a3bfbec9ead726a21
Ryan Carlson <ryan.carlson@mercsystems.ca>
2021-07-09 09:08:01 -0600

    replaced userData function and user information with arrays and loop

--- js/app.js
+++ js/app.js
@@ -1,40 +1,26 @@
-var age = 40;
-var is_subscribed = true;
+var age = ["17", "27", "17", "27"];
+var is_subscribed = ["false", "false", "true", "true"];
 
-age = 17;
-is_subscribed = false;
-userData();
-
-age = 27;
-is_subscribed = false;
-userData();
-
-age = 17;
-is_subscribed = true;
-userData();
-
-age = 27;
-is_subscribed = true;
-userData();
+for (var counter = 0; counter < age.length; counter++) {
 
 
-
-
-function userData(){
-    if (age < 18) {
+    if (age [counter] < 18) {
         console.log ("User is underage.");
-    }else if (age > 18) {
+    }else {
         console.log ("User is over the legal age.");
     } 
     
-    if (is_subscribed == true){
+    if (is_subscribed [counter] == true){
         console.log("User is subscribed");
-    }else if (is_subscribed == false){
+    }else {
         console.log("User is not subscribed");
     }
     
-    else {
-        console.log("User soes not exist")
-    }
 }
+
+//removed function userData and user information and replaced with arrays and a loop
+
+
+
+
 
